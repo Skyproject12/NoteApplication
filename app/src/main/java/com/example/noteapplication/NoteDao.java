0bd6ce089b3f104333159d1a95858853c6a1a96a
sumendra12 <sumendrakadek29@gmail.com>
2019-11-27 09:23:20 +0700

    First Commit

---
+++ app/src/main/java/com/example/noteapplication/NoteDao.java
@@ -0,0 +1,28 @@
+package com.example.noteapplication;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+public interface NoteDao {
+    @Insert(onConflict= OnConflictStrategy.IGNORE)
+    void insert(Note note);
+
+    // make function update
+    @Update
+    void update (Note note);
+
+    // delete function query
+    @Delete()
+    void delete(Note note);
+
+    // select id
+    @Query("SELECT * from note ORDER BY id ASC")
+    LiveData<List<Note>> getAllNotes();
+
+}
---
+++ app/src/main/java/com/example/noteapplication/roomDatabase.java
@@ -0,0 +1,30 @@
+package com.example.noteapplication;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+public abstract class roomDatabase extends RoomDatabase {
+
+    // initial interface dao
+    public abstract NoteDao noteDao();
+
+    // make instance
+    private static volatile roomDatabase INSTANCE;
+
+    // initial database
+    public static roomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (roomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            roomDatabase.class, "note_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
